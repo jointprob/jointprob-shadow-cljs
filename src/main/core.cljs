73a4200a2de4f33c1165435603c4352ee085bd58
James Pratt <me@jamiep.org>
2022-09-02 16:25:45 +0100

    more details on graphs

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -8,16 +8,51 @@
 (defonce random-samples (into []  (take 50 (repeatedly (fn [] (if (>= 0.6 (rand)) :w :l))))))
 (defonce samples (r/atom '()))
 
-(def coll-p (map #(/ % 200) (range 0 201)))
+(def grid-p (map #(/ % 200) (range 0 201)))
 
 (defn graph-posterior-dis [samples]
-  {:hconcat
-   [(apply g/land-or-water (d/count-land-or-water samples))
-    (g/probability-dis "Relative Likelihood" "Pr(W,L|p)" coll-p (d/r-likelihood-from-samples coll-p samples))
-    (g/probability-dis "Posterior Probability (standardized)" "Pr(p|W,L)" coll-p
-                       (->
-                        (d/r-likelihood-from-samples coll-p samples)
-                        d/standardize))]})
+  
+  (let [prior-title (if (last samples)
+                      "Prior before new sample"
+                      "Prior")
+        [n land water] (d/count-land-or-water samples)
+        [_ last-land last-water] (d/count-land-or-water (butlast samples))
+        [_ new-land new-water] (d/count-land-or-water (list (last samples)))
+        n-graph (g/land-or-water n land water)
+        prior-graph (g/probability-dis prior-title 
+                                       (str "Pr(" last-water "," last-land "|p)")
+                                       grid-p
+                                       (d/r-likelihood-from-samples  grid-p (butlast samples)))
+        eq1 (str "Pr(" new-water "," new-land "|p)")
+        new-sample-graph (g/probability-dis
+                          (str "Likelihood of new sample \"" (last samples) "\" " eq1)
+                          eq1
+                          grid-p
+                          (d/r-likelihood-from-samples grid-p (list (last samples))))
+
+        eq2 (str "Pr(" water "," land "|p)")
+        rlikelihood-graph-one-perm (g/probability-dis
+                           (str  "Relative Likelihood of This Sequence")
+                           eq2
+                           grid-p
+                           (d/r-likelihood-from-samples-simple grid-p samples))
+        rlikelihood-graph-all-perms (g/probability-dis
+                                     "Relative Likelihood of Any Sequences"
+                                     eq2
+                                     grid-p
+                                     (d/r-likelihood-from-samples grid-p samples))
+        pos-graph (g/probability-dis "Posterior Probability (standardized)"
+                                     "Pr(p|W,L)" grid-p
+                           (->
+                            (d/r-likelihood-from-samples grid-p samples)
+                            d/standardize))]
+   {:vconcat
+    (if (last samples)
+      [{:hconcat [n-graph prior-graph new-sample-graph]}
+        {:hconcat [rlikelihood-graph-one-perm rlikelihood-graph-all-perms pos-graph]}]
+      [{:hconcat [n-graph prior-graph]}
+       {:hconcat [rlikelihood-graph-one-perm rlikelihood-graph-all-perms pos-graph]}])}))
+
 
 (defn more-samples-available [samples]
   (< (count samples) (count random-samples)))
@@ -29,7 +64,7 @@
   (swap! samples one-more-sample)
 
   (if (more-samples-available @samples)
-    (js/setTimeout play 500)
+    (js/setTimeout play 1000)
     nil))
 
 (defn page []
@@ -51,12 +86,24 @@
                    (swap! samples one-more-sample))}
        (if (more-samples-available @samples) "Next sample" "Clear samples")]
       nil)
-    [:button 
-     {:onClick (fn []
-                 (js/console.log (str "Clear samples " @samples))
-                 (reset! samples '()))}
-     "Clear samples"]
-    (str "  " @samples)]])
+    (if (last @samples)
+      [:button
+       {:onClick (fn []
+                   (js/console.log (str "Remove 1 sample " @samples))
+                   (swap! samples butlast))}
+       "Remove 1 sample"]
+      nil)
+    (if (last @samples)
+      [:button
+       {:onClick (fn []
+                   (js/console.log (str "Clear samples " @samples))
+                   (reset! samples '()))}
+       "Clear samples"]
+      nil)
+    (let [[n land water] (d/count-land-or-water @samples)]
+      (str "No. of possible sequences of " water " water sample(s) and " land " land sample(s) : " 
+           (d/n-of-permutations water n)))]
+    [:div (str "  " @samples)]])
 
 (defn ^:dev/after-load start []
   (js/console.log "start")
