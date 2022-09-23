6e33f8c5270f40e807fbab42d2c24d31bc58839b
James Pratt <me@jamiep.org>
2022-09-23 14:34:06 +0100

    menu for different pages

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -1,13 +1,42 @@
 (ns core
   (:require [reagent.dom :as rdom]
-            [sampling-from-posterior]))
+            [bayes-update]
+            [sampling-from-posterior]
+            [reagent.core :as r]
+            [semantic-ui-react :as sur]))
+
+(defn page-navigation []
+  (r/with-let [showing-page (r/atom 1)]
+    [:div
+     [:> sur/Menu {:fixed "top"}
+      [:> sur/MenuItem {:as "header"} "JointProb"]
+      [:> sur/MenuItem {:as "a"
+                        :active (== @showing-page 1)
+                        :on-click (fn [e]
+                                    (.preventDefault e)
+                                    (reset! showing-page 1)
+                                    nil)}
+       "Calculate Posterior Distribution"]
+      [:> sur/MenuItem {:as "a" 
+                        :active (== @showing-page 2)
+                        :on-click (fn [e]
+                                    (.preventDefault e)
+                                    (reset! showing-page 2)
+                                    nil)} 
+       "Sampling from Posterior"]]
+     [:> sur/Container {:style {:marginTop "7em"}}
+      [:div]
+      (case @showing-page
+        1 [bayes-update/page]
+        2 [sampling-from-posterior/page])]]))
+
 
 (defn ^:dev/after-load start []
   (js/console.log "start")
-  (rdom/render [sampling-from-posterior/page]
+  (rdom/render [page-navigation]
                (. js/document (getElementById "app"))))
 
-(defn ^:export init [ & args]
+(defn ^:export init []
   (js/console.log "init")
   (start))
 
