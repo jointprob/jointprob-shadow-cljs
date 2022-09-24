a9c59687c203f337a84405033a0faff7c6978d42
James Pratt <me@jamiep.org>
2022-09-24 13:54:13 +0100

    move page state into a separate atom

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -5,29 +5,30 @@
             [reagent.core :as r]
             [semantic-ui-react :as sur]))
 
+(defonce app-state (r/atom {:showing-page 1}))
+
 (defn page-navigation []
-  (r/with-let [showing-page (r/atom 1)]
-    [:div
-     [:> sur/Menu {:fixed "top"}
-      [:> sur/MenuItem {:as "header"} "JointProb"]
-      [:> sur/MenuItem {:as "a"
-                        :active (== @showing-page 1)
-                        :on-click (fn [e]
-                                    (.preventDefault e)
-                                    (reset! showing-page 1)
-                                    nil)}
-       "Calculate Posterior Distribution"]
-      [:> sur/MenuItem {:as "a" 
-                        :active (== @showing-page 2)
-                        :on-click (fn [e]
-                                    (.preventDefault e)
-                                    (reset! showing-page 2)
-                                    nil)} 
-       "Sampling from Posterior"]]
-     [:> sur/Container {:style {:marginTop "7em"}}
-      (case @showing-page
-        1 [bayes-update/page]
-        2 [sampling-from-posterior/page])]]))
+  [:div
+   [:> sur/Menu {:fixed "top"}
+    [:> sur/MenuItem {:as "header"} "JointProb"]
+    [:> sur/MenuItem {:as "a"
+                      :active (== (:showing-page @app-state) 1)
+                      :on-click (fn [e]
+                                  (.preventDefault e)
+                                  (swap! app-state assoc-in [:showing-page] 1)
+                                  nil)}
+     "Calculate Posterior Distribution"]
+    [:> sur/MenuItem {:as "a"
+                      :active (== (:showing-page @app-state) 2)
+                      :on-click (fn [e]
+                                  (.preventDefault e)
+                                  (swap! app-state assoc-in [:showing-page] 2)
+                                  nil)}
+     "Sampling from Posterior"]]
+   [:> sur/Container {:style {:marginTop "7em"}}
+    (case (:showing-page @app-state)
+      1 [bayes-update/page]
+      2 [sampling-from-posterior/page])]])
 
 
 (defn ^:dev/after-load start []
