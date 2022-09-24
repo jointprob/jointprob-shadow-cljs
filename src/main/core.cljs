8b8d573cde86216102f90c8fb3980d8f99115a96
James Pratt <me@jamiep.org>
2022-09-24 16:39:35 +0100

    use anchors to move between pages

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -7,28 +7,34 @@
 
 (defonce app-state (r/atom {:showing-page 1}))
 
+
 (defn page-navigation []
-  [:div
-   [:> sur/Menu {:fixed "top"}
-    [:> sur/MenuItem {:as "header"} "JointProb"]
-    [:> sur/MenuItem {:as "a"
-                      :active (== (:showing-page @app-state) 1)
-                      :on-click (fn [e]
-                                  (.preventDefault e)
-                                  (swap! app-state assoc-in [:showing-page] 1)
-                                  nil)}
-     "Calculate Posterior Distribution"]
-    [:> sur/MenuItem {:as "a"
-                      :active (== (:showing-page @app-state) 2)
-                      :on-click (fn [e]
-                                  (.preventDefault e)
-                                  (swap! app-state assoc-in [:showing-page] 2)
-                                  nil)}
-     "Sampling from Posterior"]]
-   [:> sur/Container {:style {:marginTop "7em"}}
-    (case (:showing-page @app-state)
-      1 [bayes-update/page]
-      2 [sampling-from-posterior/page])]])
+  (let [anchor (.. js/window -top -location -hash (substr 1))]
+    (swap! app-state assoc-in [:showing-page] (case anchor
+                                                "bayes-update" 1
+                                                "sampling-from-posterior" 2
+                                                1))
+    [:div
+     [:> sur/Menu {:fixed "top"}
+      [:> sur/MenuItem {:as "header"} "JointProb"]
+      [:> sur/MenuItem {:as "a"
+                        :href "#bayes-update"
+                        :active (== (:showing-page @app-state) 1)
+                        :on-click (fn [e]
+                                    (swap! app-state assoc-in [:showing-page] 1)
+                                    nil)}
+       "Calculate Posterior Distribution"]
+      [:> sur/MenuItem {:as "a"
+                        :href "#sampling-from-posterior"
+                        :active (== (:showing-page @app-state) 2)
+                        :on-click (fn [e]
+                                    (swap! app-state assoc-in [:showing-page] 2)
+                                    nil)}
+       "Sampling from Posterior"]]
+     [:> sur/Container {:style {:marginTop "7em"}}
+      (case (:showing-page @app-state)
+        1 [bayes-update/page]
+        2 [sampling-from-posterior/page])]]))
 
 
 (defn ^:dev/after-load start []
